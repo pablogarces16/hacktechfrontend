921a0eac4f310c0554ae8835bfbbd23993a8f51e
Bao <chauquocbao0907@gmail.com>
2017-03-04 20:00:45 -0800

    fix http request

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-
 import {ArticlesPage} from '../articles/articles';
-
-/*import {NewsFeed} from '../../providers/news-feed/news-feed'*/
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-about',
-  /*providers: [NewsFeed],*/
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
   articlesPage = ArticlesPage;
+  
+  posts: any;
+  
 
-  console.log(this.NewsFeed.getFeed());
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public http: Http) {
+    this.http.get('https://stemy-160517.appspot.com/getNews').map(res => res.json()).subscribe(data => {
+      this.posts = data.children;
+      console.log(this.posts);
+    });
   }
 
 }
